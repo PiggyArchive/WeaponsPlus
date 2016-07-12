a0ad66954e725e280fe2e7b4887e49112204d610
Mr. Piggy <mcpepig123@gmail.com>
2016-07-12 15:35:20 -0700

    Fix format

--- src/WeaponsPlus/EventListener.php
+++ src/WeaponsPlus/EventListener.php
@@ -9,9 +9,9 @@ use pocketmine\event\Listener;
 use pocketmine\Player;
 
 class EventListener implements Listener{
-	public function __construct($plugin){
-		$this->plugin = $plugin;
-	}
+    public function __construct($plugin){
+        $this->plugin = $plugin;
+    }
 
     /**
      * @param EntityDamageEvent $event
@@ -19,31 +19,31 @@ class EventListener implements Listener{
      * @priority MONITOR
      * @ignoreCancelled true
      */
-	public function onDamage(EntityDamageEvent $event){
-    	     $entity = $event->getEntity();
-          if($entity instanceof Player){
-    	          if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player){
-    		         if(($this->plugin->getEBStatus($entity) || ($this->plugin->getConfig()->get("auto-enable-effect-blades") && !$this->plugin->getEBStatus($entity))) && $this->plugin->getConfig()->get("effect-blades")){
-                         foreach($this->plugin->getConfig()->get("effects") as $information){
-        		               $info = explode(" ", $info);
-                              $itemid = $info[0];
-                              $itemdamage = $info[1];
-        			          $effectid = $info[2];
-       			          $effectlevel = $info[3];
-        			          $effecttime = $info[4];
-        			          $particlevisible = $info[5];
-        			          $item = $event->getDamager()->getInventory()->getItemInHand();
-        			          if($item->getId() == $itemid && $item->getDamage() == $itemdamage){
-          			          $effect = Effect::getEffect($effectid);
-          			          $effect->setAmplifier($effectlevel);
-                                   $effect->setDuration($effecttime * 20);
-                                   $effect->setVisible($particlevisible);
-                                   $entity->addEffect($effect);
-        			          }
-      		          }
-    		          }
-    	          }
-          }
-     }
+    public function onDamage(EntityDamageEvent $event){
+        $entity = $event->getEntity();
+        if($entity instanceof Player){
+            if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player){
+                if(($this->plugin->getEBStatus($entity) || ($this->plugin->getConfig()->get("auto-enable-effect-blades") && !$this->plugin->getEBStatus($entity))) && $this->plugin->getConfig()->get("effect-blades")){
+                    foreach($this->plugin->getConfig()->get("effects") as $information){
+                        $info = explode(" ", $info);
+                        $itemid = $info[0];
+                        $itemdamage = $info[1];
+                        $effectid = $info[2];
+                        $effectlevel = $info[3];
+                        $effecttime = $info[4];
+                        $particlevisible = $info[5];
+                        $item = $event->getDamager()->getInventory()->getItemInHand();
+                        if($item->getId() == $itemid && $item->getDamage() == $itemdamage){
+                            $effect = Effect::getEffect($effectid);
+                            $effect->setAmplifier($effectlevel);
+                            $effect->setDuration($effecttime * 20);
+                            $effect->setVisible($particlevisible);
+                            $entity->addEffect($effect);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
