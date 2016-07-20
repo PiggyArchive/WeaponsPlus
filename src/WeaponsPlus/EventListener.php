a873d8207db5951bc75e6f580e80da29d1549b96
Mr. Piggy <mcpepig123@gmail.com>
2016-07-20 12:10:45 -0700

    Changes for landmines & grenades!
    Landmines explode when broken.
    If name-required is set to true,  landmines & grenades must have a
    custom name to function

--- src/WeaponsPlus/EventListener.php
+++ src/WeaponsPlus/EventListener.php
@@ -3,18 +3,62 @@ namespace WeaponsPlus;
 
 use pocketmine\entity\Effect;
 use pocketmine\entity\Snowball;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDespawnEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\Listener;;
+use pocketmine\event\Listener;
 use pocketmine\level\Explosion;
 use pocketmine\Player;
 
 class EventListener implements Listener {
     public function __construct($plugin) {
         $this->plugin = $plugin;
+    }
+    /**
+     * @param BlockBreakEvent $event
+     *
+     * @priority MONITOR
+     * @ignoreCancelled true
+     */
+    public function onBreak(BlockBreakEvent $event) {
+        $player = $event->getPlayer();
+        $block = $event->getBlock();
+        if($this->plugin->getConfig()->get("landmines")) {
+            if($block->getId() == $this->plugin->getConfig()->get("landmine")) {
+                $strength = $this->plugin->getConfig()->get("landmine-strength");
+                if(!is_null($this->plugin->getServer()->getPluginManager()->getPlugin("BadPiggy"))) {
+                    $explosion = new \BadPiggy\Utils\BadPiggyExplosion($block, $strength, null, $this->plugin->getServer()->getPluginManager()->getPlugin("BadPiggy"));
+                } else {
+                    $player = new Explosion($block, $strength);
+                }
+                $explosion->explodeA();
+                $explosion->explodeB();
+            }
+        }
+    }
+
+    /**
+     * @param BlockPlaceEvent $event
+     *
+     * @priority MONITOR
+     * @ignoreCancelled true
+     */
+    public function onPlace(BlockPlaceEvent $event) {
+        $player = $event->getPlayer();
+        $block = $event->getBlock();
+        if($this->plugin->getConfig()->get("landmines")) {
+            if($block->getId() == $this->plugin->getConfig()->get("landmine")) {
+                if($this->plugin->getConfig()->get("name-required")) {
+                    if(!$player->getInventory()->getItemInHand()->getCustomName() == "Landmine") {
+                        $event->setCancelled();
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -26,7 +70,8 @@ class EventListener implements Listener {
     public function onDamage(EntityDamageEvent $event) {
         $entity = $event->getEntity();
         if($entity instanceof Player) {
-            if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
+            if($event instanceof EntityDamageByEntityEvent && ($damager = $event->getDamager()) instanceof Player) {
+                $item = $damager->getInventory()->getItemInHand();
                 if($this->plugin->getEBStatus($entity) && $this->plugin->getConfig()->get("effect-blades")) {
                     foreach($this->plugin->getConfig()->get("effects") as $information) {
                         $info = explode(":", $information);
@@ -36,7 +81,6 @@ class EventListener implements Listener {
                         $effectlevel = $info[3];
                         $effecttime = $info[4];
                         $particlevisible = $info[5];
-                        $item = $event->getDamager()->getInventory()->getItemInHand();
                         if($item->getId() == $itemid && $item->getDamage() == $itemdamage) {
                             $effect = Effect::getEffect($effectid);
                             $effect->setAmplifier($effectlevel);
@@ -56,7 +100,7 @@ class EventListener implements Listener {
             $shooter = $entity->shootingEntity;
             if($shooter instanceof Player) {
                 if($this->plugin->getGrenadeStatus($shooter) && $this->plugin->getConfig()->get("grenades")) {
-                    if($shooter->getInventory()->getItemInHand()->getCustomName() == "Grenade") {
+                    if($shooter->getInventory()->getItemInHand()->getCustomName() == "Grenade" || !$this->plugin->getConfig()->get("name-required")) {
                         $strength = $this->plugin->getConfig()->get("grenade-strength");
                         if(!is_null($this->plugin->getServer()->getPluginManager()->getPlugin("BadPiggy"))) {
                             $explosion = new \BadPiggy\Utils\BadPiggyExplosion($entity, $strength, $shooter, $this->plugin->getServer()->getPluginManager()->getPlugin("BadPiggy"));
